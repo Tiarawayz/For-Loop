e7356d573e88d94aa1f304b826d450e5e135db0a
Tiara Yancey <119997013+Tiarawayz@users.noreply.github.com>
2023-09-27 00:15:03 -0400

    Add files via upload

---
+++ for_loop_basic1.py
@@ -0,0 +1,31 @@
+for i in range(0,151):
+  print(i)
+
+for i in range(5,1001,5):
+  print(i)
+
+
+  for i in range(0,101):
+    if i %10 ==0:
+      print ('Coding Dojo')
+    elif i %5 ==0:
+      print ('Coding')  
+    else:
+      print(i)
+
+
+sum = 0
+for i in range(1,500001):
+    sum += i
+    print (sum)
+
+for i in range(2018,0,-4):
+  print(i)
+
+low = 2
+high = 9
+nult = 3
+
+for i in range(low,high,+ 1):
+  if i % nult ==0:
+    print(i)
